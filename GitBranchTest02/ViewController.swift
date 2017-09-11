dece175b466d7d505512ee66562fb0c7f4eff5e0
D7702_10 <d7702_10@d7702-10ui-imac.local>
2017-09-11 14:39:23 +0900

    테스트

--- GitBranchTest02/ViewController.swift
+++ GitBranchTest02/ViewController.swift
@@ -16,11 +16,6 @@ class ViewController: UIViewController {
         print("Git Test")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
